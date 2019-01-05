1e91669c29e8cd28f04207463911fa5d101f59f2
Simbiat <simbiat@bk.ru>
2019-01-05 09:27:48 +0300

    Seconds to time

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # SandClock
-Set of functions for working with date and time. For now just formatting.
+Set of functions for working with date/time.
 
 ## How to use
-Easy:
+### Time Formatting
 ```php
 echo (new \SandClock\Api)->format(1234567890);
 ```
@@ -10,6 +10,24 @@ will output `2009-02-13 23:31:30.000000`
 
 You can format to your liking
 ```php
-echo (new \CuteBytes\Api)->setFormat('H:i')->format(1234567890);
+echo (new \SandClock\Api)->setFormat('H:i')->format(1234567890);
 ```
 to get `23:31`
+
+### Seconds to time units left
+```php
+echo (new \SandClock\Api)->format('31536050000829051');
+```
+will output `1 aeon 1 millennium 5 centuries 8 decades 5 years 6 months 1 week 1 day 7 hours 10 minutes 52 seconds`
+
+You can output the same in 'short' format (without text), for example
+```php
+echo (new \SandClock\Api)->format('31536050000829051', false);
+```
+will output `1:1:5:8:5:6:1:1:7:10:52`
+
+This function is multilingual and at the moment supports 7 languages: Deutsch (de), English (en), Spanish (es), French (fr), Italian (it), Japanese (jp), Russian (ru). You can switch it like this:
+```php
+echo (new \SandClock\Api)->format('31536050000829051', true, 'jp');
+```
+will output `1 ion 1 sennenki 5 seiki 8 tōnen 5 toshi 6 tsuki 1 shū 1 hi 7 jikan 10 bun 52 byō`
--- src/Api.php
+++ src/Api.php
@@ -5,6 +5,164 @@ namespace SandClock;
 class Api
 {
     private $dtformat = 'Y-m-d H:i:s.u';
+    const timeunits = [
+        'seconds'=>[
+	        'dependon'=>'seconds',
+	        'power'=>1,
+	        'lang'=>[
+	            'de'=>['Sekunde','Sekunden'],
+	            'en'=>['second','seconds'],
+	            'es'=>['segundo','segundos'],
+	            'fr'=>['seconde','secondes'],
+	            'it'=>['secondo','secondi'],
+	            'jp'=>['byō','byō'],
+	            'ru'=>['секунда','секунд'],
+	        ],
+        ],
+        'minutes'=>[
+	        'dependon'=>'seconds',
+	        'power'=>60,
+	        'lang'=>[
+	            'de'=>['Minute','Minuten'],
+	            'en'=>['minute','minutes'],
+	            'es'=>['minuto','minutos'],
+	            'fr'=>['minute','minutes'],
+	            'it'=>['minuto','minuti'],
+	            'jp'=>['bun','bun'],
+	            'ru'=>['минута','минут'],
+	        ],
+        ],
+        'hours'=>[
+	        'dependon'=>'minutes',
+	        'power'=>60,
+	        'lang'=>[
+	            'de'=>['Stunde','Stunden'],
+	            'en'=>['hour','hours'],
+	            'es'=>['hora','horas'],
+	            'fr'=>['heure','heures'],
+	            'it'=>['ora','ore'],
+	            'jp'=>['jikan','jikan'],
+	            'ru'=>['час','часов'],
+	        ],
+        ],
+        'days'=>[
+	        'dependon'=>'hours',
+	        'power'=>24,
+	        'lang'=>[
+	            'de'=>['Tag','Tage'],
+	            'en'=>['day','days'],
+	            'es'=>['día','días'],
+	            'fr'=>['jour','jours'],
+	            'it'=>['giorno','giorni'],
+	            'jp'=>['hi','hi'],
+	            'ru'=>['день','дней'],
+	        ],
+        ],
+        'years'=>[
+	        'dependon'=>'days',
+	        'power'=>365,
+	        'lang'=>[
+	            'de'=>['Jahr','Jahre'],
+	            'en'=>['year','years'],
+	            'es'=>['año','años'],
+	            'fr'=>['année','années'],
+	            'it'=>['anno','anni'],
+	            'jp'=>['toshi','toshi'],
+	            'ru'=>['год','лет'],
+	        ],
+        ],
+        'months'=>[
+	        'dependon'=>'days',
+	        'power'=>30,
+	        'lang'=>[
+	            'de'=>['Monat','Monate'],
+	            'en'=>['month','months'],
+	            'es'=>['mes','meses'],
+	            'fr'=>['mois','mois'],
+	            'it'=>['mese','mesi'],
+	            'jp'=>['tsuki','tsuki'],
+	            'ru'=>['месяц','месяцев'],
+	        ],
+        ],
+        'weeks'=>[
+	        'dependon'=>'days',
+	        'power'=>7,
+	        'lang'=>[
+	            'de'=>['Woche','Wochen'],
+	            'en'=>['week','weeks'],
+	            'es'=>['semana','semanas'],
+	            'fr'=>['semaine','semaines'],
+	            'it'=>['settimana','settimane'],
+	            'jp'=>['shū','shū'],
+	            'ru'=>['неделя','недель'],
+	        ],
+        ],
+        'decades'=>[
+	        'dependon'=>'years',
+	        'power'=>10,
+	        'lang'=>[
+	            'de'=>['Jahrzehnt','Jahrzehnte'],
+	            'en'=>['decade','decades'],
+	            'es'=>['década','décadas'],
+	            'fr'=>['décennie','décennies'],
+	            'it'=>['decennio','decenni'],
+	            'jp'=>['tōnen','tōnen'],
+	            'ru'=>['декада','декад'],
+	        ],
+        ],
+        'centuries'=>[
+	        'dependon'=>'decades',
+	        'power'=>10,
+	        'lang'=>[
+	            'de'=>['Jahrhundert','Jahrhunderte'],
+	            'en'=>['century','centuries'],
+	            'es'=>['siglo','siglos'],
+	            'fr'=>['siècle','siècle'],
+	            'it'=>['secolo','secoli'],
+	            'jp'=>['seiki','seiki'],
+	            'ru'=>['век','веков'],
+	        ],
+        ],
+        'millenniums'=>[
+	        'dependon'=>'centuries',
+	        'power'=>10,
+	        'lang'=>[
+	            'de'=>['Jahrtausend','Jahrtausende'],
+	            'en'=>['millennium','millennia'],
+	            'es'=>['milenio','milenio'],
+	            'fr'=>['millénaire','millénaires'],
+	            'it'=>['millennio','millenni'],
+	            'jp'=>['sennenki','sennenki'],
+	            'ru'=>['миллениум','миллениумов'],
+	        ],
+        ],
+        'megannums'=>[
+	        'dependon'=>'millenniums',
+	        'power'=>1000,
+	        'lang'=>[
+	            'de'=>['Megannum','Megannum'],
+	            'en'=>['megannum','megannums'],
+	            'es'=>['megannum','megannums'],
+	            'fr'=>['mégannum','mégannums'],
+	            'it'=>['megannum','megannum'],
+	            'jp'=>['meganamu','meganamu'],
+	            'ru'=>['мегагод','мегалет'],
+	        ],
+        ],
+        'aeons'=>[
+	        'dependon'=>'megannums',
+	        'power'=>1000,
+	        'lang'=>[
+	            'de'=>['Äon','Äonen'],
+	            'en'=>['aeon','aeons'],
+	            'es'=>['eón','eones'],
+	            'fr'=>['aeon','aeons'],
+	            'it'=>['eone','eoni'],
+	            'jp'=>['ion','ion'],
+	            'ru'=>['гигагод','гигалет'],
+	        ],
+        ],
+    ];
     
     public function format($time = 0): string
     {
@@ -25,6 +183,70 @@ class Api
             }
         }
         return (\DateTimeImmutable::createFromFormat('U.u', number_format($time, 6, '.', '')))->format($this->getFormat());
+    }
+    
+    public function seconds($seconds = 0, $full = true, $lang = 'en'): string
+    {
+        if (!is_numeric($seconds)) {
+            throw new \UnexpectedValueException('Seconds provided is not numeric.');
+        }
+        #Enforce lower case for consistency
+        $lang = strtolower($lang);
+        $units = self::timeunits;
+        #Check if langauge is supported
+        if (!in_array($lang, array_keys($units['seconds']['lang']))) {
+            throw new \UnexpectedValueException('Unsupported language (`'.$lang.'`).');
+        }
+        $result = '';
+        foreach ($units as $type=>$unit) {
+            if ($type === 'seconds') {
+                #Just adding seconds to the array to work with them going forward and explicitely converting to float (which is larger than integer) to prevent implicit conversions in following functions
+                $units[$type]['value'] = floatval($seconds);
+            } else {
+                #Calculate current unit type value based on predefined power of the dependant
+                $units[$type]['value'] = $units[$unit['dependon']]['value']/$unit['power'];
+                if ($type === 'months') {
+                    #Adjust number of days, in case we have 30 days or more, each 30 days is 1 month
+                    while (floor($units[$type]['value'])>0 && $units[$unit['dependon']]['value']>=$unit['power']) {
+                        $units[$unit['dependon']]['value'] = $units[$unit['dependon']]['value'] - $unit['power'];
+                    }
+                } else {
+                    #Deduct current unit value from the previous one in order to retain only the 'remainder' of it. 'Weeks' have an extra check for consistency between weeks, months and days
+                    if ($type !== 'weeks' || ($type === 'weeks' && floor($units[$type]['value'])>0 && $units[$unit['dependon']]['value']>=$unit['power'])) {
+                        $units[$unit['dependon']]['value'] = abs($units[$unit['dependon']]['value']-floor($units[$type]['value'])*$unit['power']);
+                    }
+                }
+                if ($type === 'weeks') {
+                    #Adjust number of weeks, in case we have 4 weeks or more, each 4 weeks is ~1 month
+                    while (floor($units['months']['value'])>0 && $units[$type]['value']>=4) {
+                        $units[$type]['value'] = $units[$type]['value'] - 4;
+                    }
+                }
+                #Add previous (already adjsuted) unit to resulting line. 'Years' and 'months' are skipped, to prevent early addition of 'days', since final value is known only on 'weeks' cycle
+                if ($type !== 'years' && $type !== 'months' && floor($units[$unit['dependon']]['value'])>0) {
+                    $result = floor($units[$unit['dependon']]['value']).($full ? ' '.(floor($units[$unit['dependon']]['value'])>1 ? $units[$unit['dependon']]['lang'][$lang][1] : $units[$unit['dependon']]['lang'][$lang][0]).' ' : ':').$result;
+                }
+                if ($type === 'weeks') {
+                    #Adding weeks
+                    if (floor($units[$type]['value'])>0) {
+                        $result = floor($units[$type]['value']).($full ? ' '.(floor($units[$type]['value'])>1 ? $units[$type]['lang'][$lang][1] : $units[$type]['lang'][$lang][0]).' ' : ':').$result;
+                    }
+                    #Adding months
+                    if (floor($units['months']['value'])>0) {
+                        $result = floor($units['months']['value']).($full ? ' '.(floor($units['months']['value'])>1 ? $units['months']['lang'][$lang][1] : $units['months']['lang'][$lang][0]).' ' : ':').$result;
+                    }
+                }
+                #Special for aeons, since last itteration
+                if ($type === 'aeons'  && floor($units[$type]['value'])>0) {
+                    $result = rtrim(trim(floor($units[$type]['value']).($full ? ' '.(floor($units[$type]['value'])>1 ? $units[$type]['lang'][$lang][1] : $units[$type]['lang'][$lang][0]).' ' : ':').$result), ':');
+                }
+            }
+            
+        }
+        if (empty($result)) {
+            $result = '0'.($full ? ' seconds' : '');
+        }
+        return $result;
     }
     
     #####################
